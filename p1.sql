bf7bb0e42ab096283a97b4c3ff070433cc548bea
saeed-pirvandi <saeidpirvandi@gmail.com>
2025-02-05 19:06:45 +0330

    -a

--- p1.sql
+++ p1.sql
@@ -6,3 +6,20 @@ FROM
     Person.Person
 WHERE
     FirstName LIKE '%[abcdef]';
+
+
+SELECT
+    a.City,
+    COUNT(DISTINCT p.BusinessEntityID) AS PeopleCount
+FROM
+    Person.Person p
+JOIN Person.BusinessEntity be
+    ON p.BusinessEntityID = be.BusinessEntityID
+JOIN Person.BusinessEntityAddress bea
+    ON be.BusinessEntityID = bea.BusinessEntityID
+JOIN Person.Address a
+    ON bea.AddressID = a.AddressID
+GROUP BY
+    a.City
+ORDER BY
+    PeopleCount DESC;
