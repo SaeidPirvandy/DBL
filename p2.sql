ef5c9610466c7affda87a4e456d7508c4071fce8
saeed-pirvandi <saeidpirvandi@gmail.com>
2025-02-05 21:32:40 +0330

    2

--- p2.sql
+++ p2.sql
@@ -15,3 +15,21 @@ END;
 
 //Execution
    EXEC SearchCommentsByText @SearchString = 'bike';
+//SQL, P2.2
+
+CREATE VIEW ProductsWithDetails
+AS
+SELECT
+    ProductID,
+    Name,
+    ProductNumber
+FROM
+    Production.Product
+WHERE
+    Style IS NOT NULL
+    OR Size IS NOT NULL
+    OR Color IS NOT NULL;
+
+//Execution
+
+   SELECT * FROM ProductsWithDetails;
