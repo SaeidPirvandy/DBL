45b95b5d6eb4f94ea94d3ff6009fdbb5a2b58bbc
saeed-pirvandi <saeidpirvandi@gmail.com>
2025-02-05 21:32:45 +0330

    4

--- p2.sql
+++ p2.sql
@@ -46,3 +46,34 @@ WHERE
     Style IS NOT NULL
     OR Size IS NOT NULL
     OR Color IS NOT NULL;
+//SQL, P2.4
+
+CREATE OR ALTER PROCEDURE GetPersonsModifiedBetweenDates
+    @StartDate DATETIME,
+    @EndDate DATETIME
+AS
+BEGIN
+    SELECT
+        BusinessEntityID AS UserID,
+        FirstName,
+        LastName,
+        ModifiedDate
+    FROM
+        Person.Person
+    WHERE
+        ModifiedDate BETWEEN @StartDate AND @EndDate
+    ORDER BY
+        ModifiedDate DESC; -- Optional: Sort by most recent
+END;
+
+//
+   EXEC GetPersonsModifiedBetweenDates
+       @StartDate = '2023-01-01',
+       @EndDate = '2023-12-31';
+   ```
+
+//Execution
+
+    UPDATE Person.Person
+    SET ModifiedDate = GETDATE() -- Sets to current date/time
+    WHERE BusinessEntityID = 1;
